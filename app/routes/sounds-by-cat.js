1ed0732d621b268190f0dff34aed1799553dd5e8
Evan <evan.burkstrand@gmail.com>
2024-12-04 16:34:40 -0600

    cat route

--- app/routes/sounds-by-cat.js
+++ app/routes/sounds-by-cat.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class SoundsByCatRoute extends Route {}
+
+export default class SoundsByCatRoute extends Route {
+
+    @service auth;
+
+    async beforeModel(){
+        await this.auth.requireLogin();
+    }
+}
