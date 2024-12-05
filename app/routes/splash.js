11b15406f73567c2de75e5040721ac6dfde52950
Hayden Carlson <haydenccarlson@gmail.com>
2024-12-04 18:11:00 -0600

    added welcome

--- app/routes/splash.js
+++ app/routes/splash.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import { doc, setDoc, getDocs, getDoc, collection } from 'firebase/firestore';
 
-export default class SplashRoute extends Route {}
+export default class SplashRoute extends Route {
+    async model() {
+        let welcome = {
+            url: '/welcome.webm',
+            name: 'welcome',
+            description: 'Here is an example',
+            id: 0,
+        }
+        return welcome;
+    }
+}
